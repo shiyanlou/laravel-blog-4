ae41960affd1ee4779d97766ed72af84aaa1825c
MiracleSnow <2210707748@qq.com>
2015-01-26 11:27:49 +0800

    upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# laravel-blog-4
+## Laravel大型项目教程：多用户博客系统（4）源代码
+
+> 出自[实验楼](www.shiyanlou.com)
---
+++ app/views/users/edit.blade.php
@@ -0,0 +1,40 @@
+@extends('_layouts.default')
+
+@section('main')
+  <div class="am-g am-g-fixed">
+    <div class="am-u-lg-6 am-u-md-8">
+      <br/>
+      @if (Session::has('message'))
+        <div class="am-alert am-alert-{{ Session::get('message')['type'] }}" data-am-alert>
+          <p>{{ Session::get('message')['content'] }}</p>
+        </div>
+      @endif
+      @if ($errors->has())
+        <div class="am-alert am-alert-danger" data-am-alert>
+          <p>{{ $errors->first() }}</p>
+        </div>
+      @endif
+      {{ Form::model($user, array('url' => 'user/' . $user->id, 'method' => 'PUT', 'class' => 'am-form')) }}
+        {{ Form::label('email', 'E-mail:') }}
+        <input id="email" name="email" type="email" readonly="readonly" value="{{ $user->email }}"/>
+        <br/>
+        {{ Form::label('nickname', 'NickName:') }}
+        <input id="nickname" name="nickname" type="text" value="{{{ Input::old('nickname', $user->nickname) }}}"/>
+        <br/>
+        {{ Form::label('old_password', 'OldPassword:') }}
+        {{ Form::password('old_password') }}
+        <br/>
+        {{ Form::label('password', 'NewPassword:') }}
+        {{ Form::password('password') }}
+        <br/>
+        {{ Form::label('password_confirmation', 'ConfirmPassword:') }}
+        {{ Form::password('password_confirmation') }}
+        <br/>
+        <div class="am-cf">
+          {{ Form::submit('Modify', array('class' => 'am-btn am-btn-primary am-btn-sm am-fl')) }}
+        </div>
+      {{ Form::close() }}
+      <br/>
+    </div>
+  </div>
+@stop
